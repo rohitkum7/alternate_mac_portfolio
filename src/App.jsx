7972c78a6f8b2a5e081c82ed6f06575217d795cc
rohit kumar <rohitkumar1215225@gmail.com>
2025-11-26 19:44:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "#components/Navbar";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Navbar />
+      </main>
+    </div>
+  );
+};
+
+export default App;
